97e6516c566944934a564c2e31e21a7bc475eb9b
Daniel Unfried <du@validio.de>
2021-03-29 11:30:23 +0200

    Move app logic to React app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div>
-      Hello, World!
-    </div>
-  );
+import React from 'react';
+import ConnectedTodos from './Todos';
+import ConnectedGoals from './Goals';
+import {connect} from 'react-redux';
+
+import {
+  handleInitialData,
+} from '../actions/shared';
+
+class App extends React.Component {
+  componentDidMount() {
+    const {dispatch} = this.props;
+
+    dispatch(handleInitialData());
+  }
+
+  render() {
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>;
+    }
+
+    return (
+      <div>
+        <ConnectedTodos/>
+        <ConnectedGoals/>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading,
+}))(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import reducer from './reducers';
+import middleware from './middleware';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+console.log(typeof reducer);
+console.log(typeof middleware);
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
